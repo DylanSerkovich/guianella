7a407d2356e1bf57d879e1bdf814794bb64d5105
Jose M BlackCat <josemblackcat@gmail.com>
2023-09-29 22:18:39 -0500

    Detalle de usuario
    Configurando detalle de usuario

---
+++ src/main/java/com/capstone/guianella/repository/impl/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package com.capstone.guianella.repository.impl;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.capstone.guianella.entity.RolEntity;
+import com.capstone.guianella.entity.UserEntity;
+
+public class CustomUserDetails implements UserDetails {
+
+    private UserEntity user;
+
+    public CustomUserDetails(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<RolEntity> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = roles.stream().map(rol -> new SimpleGrantedAuthority(rol.getName()))
+                .collect(Collectors.toList());
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnable();
+    }
+
+}

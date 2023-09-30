e88462eee813ed5a9c2911242349b10642b7b20a
Sayke28 <said.16.sometimespro@gmail.com>
2023-09-29 21:27:26 -0500

    Agregando entidades
    Creando modelos de entidad de usuario

---
+++ src/main/java/com/capstone/guianella/entity/RolEntity.java
@@ -0,0 +1,32 @@
+package com.capstone.guianella.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "rol")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RolEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rol_id")
+    private int rolId;
+    private String name;
+
+    public RolEntity(String name) {
+        this.name = name;
+    }
+
+}
